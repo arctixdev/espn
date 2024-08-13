58882119f27b3a07fbbd232da10ddfaab2aca3fe
Jonathan Bangert <jonathan@bangert.dk>
2024-08-12 19:43:09 -0600

    Some style stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>ESPN football AI agent</h1>
-      <div className="h-[80vh] min-w-[70vw] bg-secondary rounded-lg p-12 my-4 max-w-full">
+      <div className="h-[80vh] min-w-[70vw] bg-primary-foreground rounded-lg p-12 my-4 max-w-full">
         <h2>Optimal player setup for you is:</h2>
         <OptimalTable />
       </div>
