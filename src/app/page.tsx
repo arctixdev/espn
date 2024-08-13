9564d6cb400c5362e39346269256f79f8167131d
Jonathan Bangert <jonathan@bangert.dk>
2024-08-12 19:50:14 -0600

    More mobile friendly ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import { OptimalTable } from "@/lib/optimal-player";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-[5vh]">
       <h1>ESPN football AI agent</h1>
-      <div className="h-[80vh] min-w-[70vw] bg-primary-foreground rounded-lg p-12 my-4 max-w-full">
+      <div className="h-[80vh] min-w-[70vw] bg-primary-foreground rounded-lg p-12 my-4 max-w-full overflow-y-scroll">
         <h2>Optimal player setup for you is:</h2>
         <OptimalTable />
       </div>
