d800fcde4d0ac94c7a95aab076fe34f16d3f93fe
Jonathan Bangert <jonathan@bangert.dk>
2024-08-12 19:45:16 -0600

    Fix some styling on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { OptimalTable } from "@/lib/optimal-player";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-24">
       <h1>ESPN football AI agent</h1>
       <div className="h-[80vh] min-w-[70vw] bg-primary-foreground rounded-lg p-12 my-4 max-w-full">
         <h2>Optimal player setup for you is:</h2>
